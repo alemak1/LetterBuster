0a3348d22384cd097ee4b00aeb61c75a9e4311b9
yongrenwang <czie.alexmak@gmail.com>
2017-10-28 17:26:40 +0800

    first commit

---
+++ Alien Sniper Defense 3D/GameLevel.swift
@@ -0,0 +1,9 @@
+//
+//  GameLevel.swift
+//  Alien Sniper Defense 3D
+//
+//  Created by Aleksander Makedonski on 10/28/17.
+//  Copyright © 2017 Aleksander Makedonski. All rights reserved.
+//
+
+import Foundation
---
+++ Alien Sniper Defense 3D/hud/HUDManager.swift
@@ -0,0 +1,9 @@
+//
+//  HUDManager.swift
+//  Alien Sniper Defense 3D
+//
+//  Created by Aleksander Makedonski on 10/28/17.
+//  Copyright © 2017 Aleksander Makedonski. All rights reserved.
+//
+
+import Foundation
